e680c177f7e1cb0a6049f73581a167dfeb209899
Flavia Jamily dos Santos Macena <flavia.jamily.2005@gmail.com>
2024-10-04 09:25:40 -0300

    estava com a saida errada

--- questoes_itp/questao1_lista2.c
+++ questoes_itp/questao1_lista2.c
@@ -5,12 +5,11 @@ int main() {
   char caractere;
 
   // Solicita ao usuário o número de linhas e o caractere desejado
-  printf("Digite um numero inteiro e um caractere: ");
   scanf("%d %c", &linhas, &caractere);
 
 
   // Desenha o X
-  for (i = 0; i < linhas; i++) {
+  for (i = 1; i < linhas; i++) {
     for (j = 0; j < linhas; j++) {
       if (i == j || i + j == linhas - 1) {
         printf("%c", caractere);
